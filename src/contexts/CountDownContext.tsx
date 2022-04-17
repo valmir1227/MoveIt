8933fd735a3024d5cb663414a0a453b937320375
Valmir Almeida <68277445+valmir1227@users.noreply.github.com>
2022-04-17 14:26:49 -0300

    Add files via upload

--- src/contexts/CountDownContext.tsx
+++ src/contexts/CountDownContext.tsx
@@ -27,7 +27,7 @@ export const CountDownContext = createContext({} as CountDownContextData);
 export function CountDownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -42,7 +42,7 @@ export function CountDownProvider({ children }: CountdownProviderProps) {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
   }
   useEffect(() => {
     if (isActive && time > 0) {
